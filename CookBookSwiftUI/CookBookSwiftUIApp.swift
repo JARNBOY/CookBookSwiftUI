98b01c73d4c3fdf4d3ee5dbbcfc8364e3eb7a860
Papon <papon.s@lawyerplusgroup.com>
2023-09-04 09:34:49 +0700

    Initial Commit

---
+++ CookBookSwiftUI/CookBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookSwiftUIApp.swift
+//  CookBookSwiftUI
+//
+//  Created by Papon Supamongkonchai on 4/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct CookBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
