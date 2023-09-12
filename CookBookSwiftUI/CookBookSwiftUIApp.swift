7cb54b19ea721d7be3962012f5c4da45910ea12e
Papon <papon.s@lawyerplusgroup.com>
2023-09-12 09:49:57 +0700

    [CookBook App][Building Custom Tab Bar and Placing Over Window- 01] set config AppDelegate , SceneDelegate (Reviewed by Papon S.)

--- CookBookSwiftUI/CookBookSwiftUIApp.swift
+++ CookBookSwiftUI/CookBookSwiftUIApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct CookBookSwiftUIApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+//MARK: AppDelegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        return config
+    }
+}
+
+//MARK: SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    weak var windowScene: UIWindowScene?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        windowScene = scene as? UIWindowScene
     }
 }
