435ea5b92079d55a583bce9c4415659569418729
Papon <papon.s@lawyerplusgroup.com>
2023-09-22 17:05:15 +0700

    [CookBook App][Parallax Carousel Scroll] set Parallax Carousel View (Reviewed by Papon S.)

--- CookBookSwiftUI/AppView/ParallaxCarouselHScrollView.swift
+++ CookBookSwiftUI/AppView/ParallaxCarouselHScrollView.swift
@@ -41,15 +41,80 @@ struct ParallaxCarouselHScrollView: View {
                     .padding(.top, 10)
                 
                 //MARK: Parallax Carousel View
-                ScrollView(.horizontal) {
+                GeometryReader { geometry in
+                    let size = geometry.size
                     
-                }
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 10) {
+                            ///In order  to Move the Card in Reverse Direction (Parallax Effect)
+                            ForEach(places, id: \.self) { place in
+                                GeometryReader { proxy in
+                                    let cardSize = proxy.size
+                                    ///paprallax effect
+                                    let minX = proxy.frame(in: .global).minX - 30.0
+                                    
+                                    Image(place.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .scaleEffect(1.25)
+                                        .offset(x: -minX)
+                                        .frame(width: cardSize.width, height: cardSize.height, alignment: .center)
+                                        .cornerRadius(15)
+                                        .shadow(color: .black.opacity(0.25), radius: 8, x: 5, y: 10)
+                                        .overlay {
+                                            
+                                            PlaceNameView(place)
+//                                            Text("\(minX)")
+//                                                .font(.largeTitle)
+//                                                .foregroundStyle(.white)
+                                        }
+                                }
+                                .frame(width: size.width - 60, height: size.height - 50)
+                            }
+                        }//: HStack
+                        .padding(.horizontal, 30)
+                        .frame(height: size.height, alignment: .top)
+                    }//: ScrollView
+                    .scrollIndicators(.hidden)
+                }//: GeometryReader
+                .frame(height: 500)
+                .padding(.horizontal, -15)
+                .padding(.top, 10)
                 
             }//: VStack
             .padding(15)
             
         }//: ScrollView
         .scrollIndicators(.hidden)
+    }
+    
+    @ViewBuilder
+    func PlaceNameView(_ place: Destination) -> some View {
+        ZStack(alignment: .bottomLeading) {
+            LinearGradient(colors: [
+                .clear,
+                .clear,
+                .clear,
+                .clear,
+                .clear,
+                .black.opacity(0.1),
+                .black.opacity(0.5),
+                .black
+            ], startPoint: .top, endPoint: .bottom)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(place.country)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                
+                Text(place.place)
+                    .font(.callout)
+                    .foregroundStyle(.white.opacity(0.8))
+            }
+            .padding(20)
+        }
+        .cornerRadius(15)
     }
 }
 
