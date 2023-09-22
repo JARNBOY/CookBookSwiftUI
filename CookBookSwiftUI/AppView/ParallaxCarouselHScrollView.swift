13f625f6ce198b1cf7de7d3ada1b5a2df561c21f
Papon <papon.s@lawyerplusgroup.com>
2023-09-22 16:11:33 +0700

    [CookBook App][Parallax Carousel Scroll] set up model , data (Reviewed by Papon S.)

--- CookBookSwiftUI/AppView/ParallaxCarouselHScrollView.swift
+++ CookBookSwiftUI/AppView/ParallaxCarouselHScrollView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct ParallaxCarouselHScrollView: View {
+    
+    @State private var searchText: String = ""
+    let places: [Destination] = honeymoonData
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical) {
+            VStack(spacing: 15) {
+                
+                //MARK: Search View
+                HStack(spacing: 12) {
+                    Button {} label: {
+                        Image(systemName: "line.3.horizontal")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }//: Button
+                    
+                    HStack(spacing: 12) {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Search", text: $searchText)
+                    }//: HStack
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 10)
+                    .background(.ultraThinMaterial, in: Capsule())
+                }//: HStack
+                
+                Text("Where do you want to \ntravel?")
+                    .font(.largeTitle.bold())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 10)
+                
+                //MARK: Parallax Carousel View
+                ScrollView(.horizontal) {
+                    
+                }
+                
+            }//: VStack
+            .padding(15)
+            
+        }//: ScrollView
+        .scrollIndicators(.hidden)
     }
 }
 
---
+++ CookBookSwiftUI/Models/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  CookBookSwiftUI
+//
+//  Created by Papon Supamongkonchai on 22/9/2566 BE.
+//
+
+import SwiftUI
+
+struct Destination {
+  var place: String
+  var country: String
+  var image: String
+}
